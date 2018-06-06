05147d2f12db09ff4f7bccbc79c0fe6a6363dcfa
rocketflinn <30417713+rocketflinn@users.noreply.github.com>
2018-06-06 11:43:50 -0400

    Asset created by rocketflinn

---
+++ code/libraries/globals/globals.js
@@ -0,0 +1,9 @@
+const GEOFENCETABLE="geofences";
+const GEOFENCETOPIC="geoFences/list";
+var _resp;
+var _req;
+//var DEVICES = "yepzondevices";
+var DEVICES = "arknavdevices";
+var TWILLIOSID="AC218b72987d86853c5adb921370115a20";
+var TWILLIOAUTH="4579ca6ba4fae7b232c03c64aeae40e7";
+var TWILLIOPHONE="5127102383";
